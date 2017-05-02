60b4b2436fd7303558f8c281bb8b7110f22dc8e6
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-05-02 21:52:00 +0300

    -- Add directive to equals

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/ImportPath.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/ImportPath.kt
@@ -39,37 +39,17 @@ val Import.importedName: Name? get() {
 }
 
 fun Import.getText(): String {
-    val fqNameStr = fqName.toUnsafe().render() ?: return ""
+    val fqNameStr = fqName.toUnsafe().render()
     val pathStr = fqNameStr + if (isAllUnder) ".*" else ""
     return pathStr + if (alias != null && !isAllUnder) (" as " + alias?.asString()) else ""
 }
 
-class ImportPath @JvmOverloads constructor(
+data class ImportPath @JvmOverloads constructor(
         override val fqName: FqName,
         override val isAllUnder: Boolean,
         override val alias: Name? = null,
         override val importDirective: KtImportDirective? = null): Import {
     override fun toString(): String = getText()
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other?.javaClass != javaClass) return false
-
-        other as ImportPath
-
-        if (fqName != other.fqName) return false
-        if (isAllUnder != other.isAllUnder) return false
-        if (alias != other.alias) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = fqName.hashCode()
-        result = 31 * result + isAllUnder.hashCode()
-        result = 31 * result + (alias?.hashCode() ?: 0)
-        return result
-    }
 
     companion object {
         @JvmStatic fun fromString(pathStr: String): ImportPath {
