9b277d83c824668f6631b3b0750379946ec7e32c
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-05-03 16:49:50 +0300

    -- revert equality for imports

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/ImportPath.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/ImportPath.kt
@@ -44,12 +44,32 @@ fun Import.getText(): String {
     return pathStr + if (alias != null && !isAllUnder) (" as " + alias?.asString()) else ""
 }
 
-data class ImportPath @JvmOverloads constructor(
+class ImportPath @JvmOverloads constructor(
         override val fqName: FqName,
         override val isAllUnder: Boolean,
         override val alias: Name? = null,
         override val importDirective: KtImportDirective? = null): Import {
     override fun toString(): String = getText()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+
+        other as ImportPath
+
+        if (fqName != other.fqName) return false
+        if (isAllUnder != other.isAllUnder) return false
+        if (alias != other.alias) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = fqName.hashCode()
+        result = 31 * result + isAllUnder.hashCode()
+        result = 31 * result + (alias?.hashCode() ?: 0)
+        return result
+    }
 
     companion object {
         @JvmStatic fun fromString(pathStr: String): ImportPath {
